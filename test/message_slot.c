09db78220116218df82d9f85c2e5eecfe3ec21e7
sapiraviv <92387988+sapiraviv@users.noreply.github.com>
2023-05-30 17:25:22 +0300

    Update message_slot.c

--- test/message_slot.c
+++ test/message_slot.c
@@ -68,6 +68,8 @@ static int device_release( struct inode* inode, struct file*  file){
 void make_first_channel(ms_channel *curr_chanel, ms_channel *prev, ms_file *node, ms_channel *old_first){
     node->first = curr_chanel;
     if (old_first == NULL){
+        curr_chanel->next = NULL;
+        printk("here");
         return;
     }
     prev->next = curr_chanel->next;
